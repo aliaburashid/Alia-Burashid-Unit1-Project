89daeb614c68b62de773147355dcdec00ade9c7e
aliaburashid <fy22ayyy@leeds.ac.uk>
2025-07-14 01:17:37 +0300

    Create cards and Implement flip logic

--- memory-game/js/app.js
+++ memory-game/js/app.js
@@ -11,3 +11,32 @@ function selectCategory(element) {
   document.querySelector('.levels').style.display = 'block';
 }
 
+
+// cards.html
+  // define the card values
+        const letters = ["A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F", "F", "G", "G", "H", "H"]
+
+        // commonly used trick to shuffle an array randomly 
+        // Math.random gives a range between 0-1 do 0.5 is neutral 
+        // .sort() compares each pair of items: + swap, -keep, 0 nothing 
+        const shuffledLetters = letters.sort(() => Math.random() - 0.5);
+
+        // find the class game and save it as the const varaiable so you can put cards inside later 
+        const gameContainer = document.querySelector('.game');
+
+        // Loop over each letter to create a card (div)
+         for (let i = 0; i < shuffledLetters.length; i++) {
+            // Create a new (div)const for card box 
+            const card = document.createElement('div');
+            
+            card.className = 'item'; // gives the card a class item to style 
+            
+            card.textContent = shuffledLetters[i]; // places a letter inise the card 
+
+            card.addEventListener('click', () => {
+                card.classList.toggle('flipCard'); // Flip card on click
+            });
+
+            gameContainer.appendChild(card); // adds the created card into the board
+    }
+
