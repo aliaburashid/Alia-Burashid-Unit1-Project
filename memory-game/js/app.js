37ae7cabc9a1fcd52952530849534ade893f1dd1
aliaburashid <fy22ayyy@leeds.ac.uk>
2025-07-14 01:59:46 +0300

    Fix card flip logic and ensure match check only runs after two selections

--- memory-game/js/app.js
+++ memory-game/js/app.js
@@ -24,6 +24,13 @@ function selectCategory(element) {
         // find the class game and save it as the const varaiable so you can put cards inside later 
         const gameContainer = document.querySelector('.game');
 
+        // reset the selection after comparing the cards, so the next ones can be stored again 
+        // If you don’t reset them, the program would keep comparing the same old cards
+        let firstCard = null;
+        let secondCard = null;
+        // click is back to interactive after waiting for a comparisonn 
+        let preventClick = false;
+
         // Loop over each letter to create a card (div)
          for (let i = 0; i < shuffledLetters.length; i++) {
             // Create a new (div)const for card box 
@@ -34,7 +41,38 @@ function selectCategory(element) {
             card.textContent = shuffledLetters[i]; // places a letter inise the card 
 
             card.addEventListener('click', () => {
+                // If not clickable or already flipped, ignore click
+                if (preventClick || card.classList.contains('flipCard')) return;
+
                 card.classList.toggle('flipCard'); // Flip card on click
+
+                //  if its empty (meaning no card is stored) then save the card clicked as firstCard
+                // else, if we akready have a first card then save it as secondCard and prevent clicks untill comparison 
+                if (!firstCard) {
+                    firstCard = card;
+                } else  {
+                    secondCard= card;
+                    preventClick = true;
+                }
+
+                // checks if the letters inside both cards are the same 
+                if (firstCard.textContent === secondCard.textContent) {
+                    // If match found then keep both cards flipped 
+                    // set variables to null for new selectors and make cards clickable
+                    firstCard = null;
+                    secondCard = null;
+                    preventClick = false;
+                } else {
+                    // if the cards dont match, flip back after short delay 
+                     setTimeout(() => {
+                        // flip crads back by removing the class
+                        firstCard.classList.remove('flipCard');
+                        secondCard.classList.remove('flipCard');
+                        firstCard = null;
+                        secondCard = null;
+                        preventClick= false;
+                    }, 800); // 0.8 seconds
+                }
             });
 
             gameContainer.appendChild(card); // adds the created card into the board
